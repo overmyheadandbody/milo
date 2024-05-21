e3fb9bd0514403dde3db9a9e3c0e5f8842be2b83
Rares Munteanu <overmyheadandbody@gmail.com>
2024-05-21 14:13:49 +0200

    Update high-impact-alert.js

--- .github/workflows/high-impact-alert.js
+++ .github/workflows/high-impact-alert.js
@@ -5,13 +5,14 @@ const {
 
 const main = async (params) => {
   const { context } = params;
+  console.log('Context is', context);
 
   try {
     if (context.payload.label.name === 'high-impact') {
       const { html_url, number, title } = context.payload.pull_request;
       console.log('High impact label detected, sending Slack notification');
-      slackNotification(`:alert: High Impact PR has been opened: <${html_url}|#${number}: ${title}>.` +
-         ` Please prioritize testing the proposed changes.`, process.env.MILO_COMMUNITY_SLACK_WEBHOOK);
+      /*slackNotification(`:alert: High Impact PR has been opened: <${html_url}|#${number}: ${title}>.` +
+         ` Please prioritize testing the proposed changes.`, process.env.MILO_COMMUNITY_SLACK_WEBHOOK);*/
     } else {
       console.log('No high impact label detected');
     }
